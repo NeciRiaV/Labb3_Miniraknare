1bbdb9d0877101ebdaeaa0e09fe3b1e9e22c68d0
vitoria borg vidal <vitoriaborgvidal1998@gmail.com>
2022-07-03 01:11:25 +0200

    Skapade första testet med hjälp av Theory.

--- Labb3_MiniräknareXUnitTest/CalculationsTest.cs
+++ Labb3_MiniräknareXUnitTest/CalculationsTest.cs
@@ -7,6 +7,29 @@ namespace Labb3_MiniräknareXUnitTest
 {
     public class CalculationsTest
     {
-        
+        //ADDITION
+        [Theory]
+        [InlineData(20, 20.5, 40.5, true)]
+        [InlineData(10, -20, 30, true)]
+        [InlineData(30.8, 5.5, 36.3, true)]
+        [InlineData(5, -3, -2, false)]
+        [InlineData(10, 3, 12, false)]
+        public void Test_Addition(double num1, double num2, double result, bool isItTrue)
+        {
+            var calc = new Calculations();
+
+            var isValid = calc.Addition(num1, num2);
+            var actual = isValid == result;
+
+            Assert.Equal(isItTrue, actual);
+        }
+
+        //SUBTRAKTION
+
+
+        //DIVISION
+
+
+        //MULTIPLIKATION
     }
 }
