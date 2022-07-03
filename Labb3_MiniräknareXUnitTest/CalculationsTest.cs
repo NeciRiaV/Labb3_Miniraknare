d40fc048c7dbe338c1f674f1c8667e49557ab856
vitoria borg vidal <vitoriaborgvidal1998@gmail.com>
2022-07-04 00:46:28 +0200

    Skapade test för att se om Visa kalkulationer metoden funkar.

--- Labb3_MiniräknareXUnitTest/CalculationsTest.cs
+++ Labb3_MiniräknareXUnitTest/CalculationsTest.cs
@@ -16,11 +16,14 @@ namespace Labb3_MiniräknareXUnitTest
         [InlineData(10, 3, 12, false)]
         public void Test_Addition(double num1, double num2, double result, bool isItTrue)
         {
+            //Arrange
             var calc = new Calculations();
 
+            //Act
             var isValid = calc.Addition(num1, num2);
             var actual = isValid == result;
 
+            //Assert
             Assert.Equal(isItTrue, actual);
         }
 
@@ -28,21 +31,27 @@ namespace Labb3_MiniräknareXUnitTest
         [Fact]
         public void Test_Subtraction_Positive_Result()
         {
+            //Arrange
             var posNum = new Calculations();
 
+            //Act
             var actual = posNum.Subtraction(4, 2);
             var expected = 2;
 
+            //Assert
             Assert.Equal(actual, expected);
         }
         [Fact]
         public void Test_Subtraction_Negative_Result()
         {
+            //Arrange
             var negNum = new Calculations();
 
+            //Act
             var actual = negNum.Subtraction(-10, 5);
             var expected = -15;
 
+            //Assert
             Assert.Equal(actual, expected);
         }
 
@@ -52,21 +61,27 @@ namespace Labb3_MiniräknareXUnitTest
         [Fact]
         public void Test_Division_Positive_Result()
         {
+            //Arrange
             var posNum = new Calculations();
 
+            //Act
             var actual = posNum.Division(50, 2);
             var expected = 25;
 
+            //Assert
             Assert.Equal(actual, expected);
         }
         [Fact]
         public void Test_Division_Negative_Result()
         {
+            //Arrange
             var negNum = new Calculations();
 
+            //Act
             var actual = negNum.Division(-20, 2);
             var expected = -10;
 
+            //Assert
             Assert.Equal(actual, expected);
         }
         [Theory]
@@ -75,11 +90,14 @@ namespace Labb3_MiniräknareXUnitTest
         [InlineData(5, 0.25, 20, true)]
         public void Test_Division_With_0(double num1, double num2, double result, bool isItTrue)
         {
+            //Arrange
             var calc = new Calculations();
 
+            //Act
             var isValid = calc.Division(num1, num2);
             var actual = isValid == result;
 
+            //Assert
             Assert.Equal(isItTrue, actual);
         }
 
@@ -88,24 +106,57 @@ namespace Labb3_MiniräknareXUnitTest
         [Fact]
         public void Test_Multiplication_Positive_Result()
         {
+            //Arrange
             var num = new Calculations();
 
+            //Act
             var actual = num.Multiplication(3, 3);
             var expected = 9;
 
+            //Assert
             Assert.Equal(actual, expected);
         }
 
         [Fact]
         public void Test_Multiplication_Negative_Result()
         {
+            //Arrange
             var negNum = new Calculations();
 
+            //Act
             var actual = negNum.Multiplication(-3, 3);
             var expected = -9;
 
+            //Assert
             Assert.Equal(actual, expected);
         }
+
+
+
+        //LISTA
+        [Theory]
+        [InlineData("2 + 3 = 5", true)]
+        [InlineData("1 x 3 = 3", true)]
+        [InlineData("-2 - 3 = -5", true)]
+        [InlineData("15 / 5 = 3", true)]
+        [InlineData("16 + 5 = 21", false)]
+        public void Test_ShowCalculations(string expected, bool isItTrue)
+        {
+            //Arrange
+            var c = new Calculations();
+            c.ListOfCalculations.Add("2 + 3 = 5");
+            c.ListOfCalculations.Add("1 x 3 = 3");
+            c.ListOfCalculations.Add("-2 - 3 = -5");
+            c.ListOfCalculations.Add("15 / 5 = 3");
+
+            //Act
+            var isValid = c.ShowCalculations();
+            var actual = isValid == expected;
+
+            //Assert
+            Assert.Equal(isItTrue, actual);
+        }
+
 
     }
 }
